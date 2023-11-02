226669add03c81ee66f55bfeeaed31f9e2e9f781
Faioa <dartoismatthieu.pro@gmail.com>
2023-11-02 22:27:45 +0100

    🔨 Start of AI development

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -1,10 +1,19 @@
 package logic;
 
+import grid.Grid;
+
 public abstract class Player {
 
 	private String name;
+	private Grid g;
 
-	public Player(String name) {
+	public Player(String name, Grid g) {
 		this.name = name;
+	}
+
+	public void play() {
+		/*
+		 * Scanners ?
+		 */
 	}
 }
---
+++ src/logic/PlayerAI.java
@@ -0,0 +1,19 @@
+package logic;
+
+import grid.Grid;
+
+public class PlayerAI extends Player {
+
+	public PlayerAI(String name, Grid g) {
+		super(name, g);
+	}
+
+	@Override
+	public void play() {
+		/*
+		 * Algorithme de recherche IA avec elagage alpha beta etc
+		 */
+		// Appeler la methode pour bouger les pieces
+	}
+
+}

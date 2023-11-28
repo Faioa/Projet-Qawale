c7f1ffcf2bf0b8eb2802ee1a6f09b7e9f4974fd2
Faioa <dartoismatthieu.pro@gmail.com>
2023-11-28 11:06:39 +0100

    Changing PC commit to keep changes

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -2,21 +2,38 @@ package logic;
 
 import grid.Grid;
 
-public abstract class Player {
+public class Player {
 
 	private String name;
+	private Grid grid;
 
-	public Player(String name) {
+	public Player(String name, Grid grid) {
 		this.name = name;
+		this.grid = grid;
 	}
-	
-	public abstract boolean isVide();
-	
-	public abstract void putPiece(Grid g, int x, int y);
-	
+
+	public boolean isVide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void putPiece(int x, int y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getNbPiece() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void play() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
 	public String toString() {
 		return name;
 	}
-	
-	public abstract int getNbPiece();
 }
